ab66cc4965697a61d27a09dc1b6593f2432dbb78
CoderEven <huangjinwen@JinwendeMacBook-Pro.local>
2016-07-27 00:59:29 +0800

    自定义NSLog(func NSLog)
    自定义NSLog(func NSLog)

--- Weibo/Weibo/AppDelegate.swift
+++ Weibo/Weibo/AppDelegate.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 @UIApplicationMain
+
+/**
+项目分为开发阶段和发布阶段，开始阶段自动显示LOG, 发布阶段自己屏蔽LOG
+开发阶段: 需要显示LOG
+发布阶段: 不需要显示LOG
+
+文件名称-方法名称[行号]: 输出内容
+print(__FILE__) // 拿到当前文件路径
+print(__LINE__) // 拿到当前的行号
+print(__FUNCTION__) // 拿到当前的方法名称
+*/
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+
         // 1.创建window
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.orangeColor()
@@ -48,7 +60,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
        
     }
-
-
 }
+
+// <T>: 为泛型，外界传入什么就是什么
+func NSLog<T>(message: T, file: NSString = __FILE__, method: String = __FUNCTION__, line: Int = __LINE__) {
+    
+    // Weibo -> BuildSetting -> Swift Compiler custom flag -> DEBUG(添加-D DEBUG)
+    #if DEBUG
+    print("\(method) \(line) \(message)")
+    #endif
+}
+
+
+
+
+
+
+
 
